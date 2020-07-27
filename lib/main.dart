f517a3320f6e7ddc97b9b025f6b3351728b2f7bd
Luan Andrade <luansandrade92@hotmail.com>
2020-07-27 14:24:11 -0300

    Extrai Lista e Item de transferencia para classes novas usando StatelessWidget
    StatefulWidget: Consegue modificar o conteúdo do Widget de maneira dinamica
    Stateless: Não consegue modificar o conteúdo

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("100.0"),
-                subtitle: Text("1000.0"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("200.0"),
-                subtitle: Text("1000.0"),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(
           title: Text("Transferências"),
         ),
@@ -30,3 +13,46 @@ void main() => runApp(
       ),
     ),
 );
+
+// StatefulWidget: Consegue modificar o conteúdo do Widget
+// de maneira dinamica
+
+// Stateless: Não consegue modificar o conteúdo
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 1001)),
+          ItemTransferencia(Transferencia(300.0, 2000))
+        ]
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+}
