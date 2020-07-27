37dcb9a9654f18b865832cde8166710b537ff258
Luan Andrade <luansandrade92@hotmail.com>
2020-07-27 16:40:07 -0300

    Utiliza widget ListView para poder scrollar a lista de transferencias

--- lib/main.dart
+++ lib/main.dart
@@ -66,18 +66,21 @@ class FormularioTransferencia extends StatelessWidget {
 
 // Stateless: Não consegue modificar o conteúdo
 class ListaTransferencias extends StatelessWidget {
+
+  final List<Transferencia> _transferencias = List();
   @override
   Widget build(BuildContext context) {
+    _transferencias.add(Transferencia(100.0, 123));
     return Scaffold(
       appBar: AppBar(
         title: Text("Transferências"),
       ),
-      body: Column(
-          children: [
-            ItemTransferencia(Transferencia(100.0, 1000)),
-            ItemTransferencia(Transferencia(200.0, 1001)),
-            ItemTransferencia(Transferencia(300.0, 2000))
-          ]
+      body: ListView.builder(
+        itemCount: _transferencias.length,
+        itemBuilder: (context, indice) {
+          final Transferencia transferencia = _transferencias[indice];
+          return ItemTransferencia(transferencia);
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -93,6 +96,7 @@ class ListaTransferencias extends StatelessWidget {
           future.then((transferenciaRecebida) {
             debugPrint('chegou no then do future');
             debugPrint('$transferenciaRecebida');
+            _transferencias.add(transferenciaRecebida);
           });
         },
       ),
