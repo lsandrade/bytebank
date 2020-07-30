11a6d19069e2d1f4b5314510ee838c222397b81f
Luan Andrade <luansandrade92@hotmail.com>
2020-07-30 14:09:11 -0300

    Usa async e await para facilitar retorno de callbacks

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,42 +2,46 @@ import 'package:bytebank/models/contato.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath){
-    final String path = join(dbPath, "bytebank.db");
-    return openDatabase(
+const DATABASE = "bytebank.db";
+const TABLE_CONTACTS = "contacts";
+
+Future<Database> getDatabase() async {
+  String path = join(await getDatabasesPath(), DATABASE);
+
+  return openDatabase(
         path,
         onCreate: (db, version) {
-          db.execute("CREATE TABLE contacts("
+          db.execute("CREATE TABLE $TABLE_CONTACTS("
               "id INTEGER PRIMARY KEY,"
               "name TEXT,"
               "account INTEGER)");
         },
     version: 1,
     onDowngrade: onDatabaseDowngradeDelete);
-  });
 }
 
-Future<int> save(Contato contato) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contatoMap = Map();
+Future<int> save(Contato contato) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contatoMap = Map();
 
-    contatoMap['name'] = contato.name;
-    contatoMap['account'] = contato.account;
+  contatoMap['name'] = contato.name;
+  contatoMap['account'] = contato.account;
 
-    return db.insert("contacts", contatoMap);
-  });
+  return db.insert(TABLE_CONTACTS, contatoMap);
 }
 
-Future<List<Contato>> findAll() {
-  return createDatabase().then((db) {
-    return db.query("contacts").then((maps) {
-      final List<Contato> contatos = List();
-      for(Map<String, dynamic> map in maps) {
-        final Contato contato = Contato(map['id'], map['name'], map['account']);
+Future<List<Contato>> findAll() async {
+  final Database db = await getDatabase();
+
+  final List<Map<String, dynamic>> result = await db.query(TABLE_CONTACTS);
+
+  final List<Contato> contatos = List();
+  for(Map<String, dynamic> row in result) {
+        final Contato contato = Contato(
+            row['id'],
+            row['name'],
+            row['account']);
         contatos.add(contato);
-      }
-      return contatos;
-    });
-  });
+  }
+  return contatos;
 }
