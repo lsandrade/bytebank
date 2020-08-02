e3ef258840a3469adc9cba6eb47319f1f356c85c
Luan Andrade <luansandrade92@hotmail.com>
2020-08-02 15:08:47 -0300

    Salva transferencia

--- lib/webapi/webclient.dart
+++ lib/webapi/webclient.dart
@@ -1,10 +1,11 @@
-
 import 'dart:convert';
 
 import 'package:bytebank/models/contato.dart';
 import 'package:bytebank/models/transferencia.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
+
+const String baseUrl = "http://192.168.15.17:8080/transactions";
 
 class LoggingInterceptor implements InterceptorContract {
   @override
@@ -28,27 +29,61 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client =
+    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+
 Future<List<Transferencia>> findAll() async {
-  final Client client = HttpClientWithInterceptor.build(
-      interceptors: [LoggingInterceptor()]
-  );
-  final Response response = await client.get("http://192.168.15.17:8080/transactions").timeout(Duration(seconds: 5));
+  final Response response =
+      await client.get(baseUrl).timeout(Duration(seconds: 5));
   print("FindAll:");
   print(response.body);
   print("================");
   final List<dynamic> decoded = jsonDecode(response.body);
   final List<Transferencia> transferencias = List();
   for (Map<String, dynamic> transferenciaJson in decoded) {
-    final Transferencia transferencia = Transferencia(
-        transferenciaJson['value'],
-          Contato(
-              0,
-              transferenciaJson['contact']['name'],
-              transferenciaJson['contact']['accountNumber']
-        )
-    );
+    final Transferencia transferencia = jsonParaTransferencia(transferenciaJson);
     transferencias.add(transferencia);
   }
   print(transferencias);
   return transferencias;
 }
+
+Future<Transferencia> save(Transferencia transferencia) async {
+  String transferenciaJson = transferenciaParaJson(transferencia);
+
+  final Response response = await client.post(
+      baseUrl,
+      headers: {
+        "Content-Type": "application/json",
+        "password": "1000"
+      },
+    body: transferenciaJson
+  );
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  return jsonParaTransferencia(json);
+
+}
+
+String transferenciaParaJson(Transferencia transferencia) {
+  final Map<String, dynamic> transferenciaMap = {
+    "value": transferencia.valor,
+    "contact": {
+      "name": transferencia.contato.name,
+      "accountNumber": transferencia.contato.account
+    }
+  };
+
+  final String transferenciaJson = jsonEncode(transferenciaMap);
+  return transferenciaJson;
+}
+
+Transferencia jsonParaTransferencia(Map<String, dynamic> json) {
+  return Transferencia(json['value'],
+      Contato(0,
+          json['contact']['name'],
+          json['contact']['accountNumber']
+      )
+  );
+}
+
